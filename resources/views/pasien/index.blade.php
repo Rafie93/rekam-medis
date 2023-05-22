702b87b2ce9ec6be8a2b31e5b25b75eac676a93c
rafie <rafie@the-netwerk.com>
2023-05-22 14:48:29 +0800

    LABEL D

--- resources/views/pasien/index.blade.php
+++ resources/views/pasien/index.blade.php
@@ -56,7 +56,7 @@
                             @foreach ($datas as $key=>$row)
                                 <tr>
                                     <td>{{$datas->firstItem() + $key}}</td>
-                                   <td><a href="{{Route('rekam.detail',$row->id)}}">RM#{{$row->no_rm}}</a></td>
+                                   <td><a href="{{Route('rekam.detail',$row->id)}}">{{$row->no_rm}}</a></td>
                                    <td>{{$row->nama}}</td>
                                    <td>{{$row->tmp_lahir}},{{$row->tgl_lahir}}</td>
                                    <td>{{$row->alamat_lengkap}}</td>
