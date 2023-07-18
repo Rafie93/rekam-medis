4a75114c8f3682ef6281c923047589f95ebec695
rafie <rafie@the-netwerk.com>
2023-07-18 09:23:26 +0800

    status <=  2

--- resources/views/rekam/detail-rekam.blade.php
+++ resources/views/rekam/detail-rekam.blade.php
@@ -265,11 +265,9 @@
                                                         <td> {{$item->diagnosis->code}}</td>
                                                         <td>
                                                             
-                                                        @if (($rekamLatest))
-                                                            @if (($rekamLatest->status <= 2))
+                                                        @if (($row->status<=2))
                                                             <a href="{{Route('rekam.diagnosa.delete',$item->id)}}" class="btn btn-danger shadow btn-xs sharp">
                                                                 <i class="fa fa-trash"></i>   </a>
-                                                             @endif
                                                         @endif
                                                         </td>
                                                     </tr>
